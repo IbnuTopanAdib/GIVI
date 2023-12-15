1429cb7f064b68847be6607e02e1848e51feb0f9
Ibnu Topan <99580553+IbnuTopanAdib@users.noreply.github.com>
2023-12-16 02:54:28 +0700

    fix: fix bug in favorites (#20)

--- resources/views/donations/index.blade.php
+++ resources/views/donations/index.blade.php
@@ -246,36 +246,6 @@
                         </a>
                     </div>
                 @endforeach
-                {{-- <div class="col-md-4">
-                    <a href="/" class="card-link">
-                        <div class="card-barang">
-                            <div class="icon">
-                                <svg xmlns="http://www.w3.org/2000/svg" height="38" width="38"
-                                    viewBox="0 0 512 512">
-                                    <path
-                                        d="M465 7c-9.4-9.4-24.6-9.4-33.9 0L383 55c-2.4 2.4-4.3 5.3-5.5 8.5l-15.4 41-77.5 77.6c-45.1-29.4-99.3-30.2-131 1.6c-11 11-18 24.6-21.4 39.6c-3.7 16.6-19.1 30.7-36.1 31.6c-25.6 1.3-49.3 10.7-67.3 28.6C-16 328.4-7.6 409.4 47.5 464.5s136.1 63.5 180.9 18.7c17.9-17.9 27.4-41.7 28.6-67.3c.9-17 15-32.3 31.6-36.1c15-3.4 28.6-10.5 39.6-21.4c31.8-31.8 31-85.9 1.6-131l77.6-77.6 41-15.4c3.2-1.2 6.1-3.1 8.5-5.5l48-48c9.4-9.4 9.4-24.6 0-33.9L465 7zM208 256a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
-                                </svg>
-                            </div>
-                            <p class="title">Alat Musik</p>
-                            <p class="text">Alat musik yang masih layak digunakan</p>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-md-4">
-                    <a href="/" class="card-link">
-                        <div class="card-barang">
-                            <div class="icon">
-                                <svg xmlns="http://www.w3.org/2000/svg" height="38" width="38"
-                                    viewBox="0 0 448 512">
-                                    <path
-                                        d="M96 0C43 0 0 43 0 96V416c0 53 43 96 96 96H384h32c17.7 0 32-14.3 32-32s-14.3-32-32-32V384c17.7 0 32-14.3 32-32V32c0-17.7-14.3-32-32-32H384 96zm0 384H352v64H96c-17.7 0-32-14.3-32-32s14.3-32 32-32zm32-240c0-8.8 7.2-16 16-16H336c8.8 0 16 7.2 16 16s-7.2 16-16 16H144c-8.8 0-16-7.2-16-16zm16 48H336c8.8 0 16 7.2 16 16s-7.2 16-16 16H144c-8.8 0-16-7.2-16-16s7.2-16 16-16z" />
-                                </svg>
-                            </div>
-                            <p class="title">Buku</p>
-                            <p class="text">Buku yang masih layak digunakan</p>
-                        </div>
-                    </a>
-                </div> --}}
             </div>
         </div>
 
